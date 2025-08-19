39c045b39252388cbe0461b139ed63081c0a1ba1
Kanishkare <duvvurukanishka@gmail.com>
2025-08-19 23:02:45 +0530

    commit all changes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app=express();
 const port=8080;
 const path=require('path');
 const{v4:uuidv4}=require('uuid');
+const methodOverride = require('method-override');
 
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -60,9 +62,20 @@ app.patch("/posts/:id",(req,res)=>{
     let post=posts.find((p)=>id===p.id);
     post.content=newcontent;
     console.log(post);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
 
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post=posts.find((p)=>id===p.id);
+    res.render("edit.ejs",{post});
+})
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} =req.params;
+    posts=posts.filter((p)=>id!==p.id);
+    res.redirect("/posts");
+})
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
